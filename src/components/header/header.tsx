98b2ce8d9d142ecb4a3a9fd7debe9d7690fe4a79
alterrant <pandor246@mail.ru>
2022-10-05 23:55:19 +0300

    add(header): drop down menu lists

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,18 +1,31 @@
+import { useNavigate } from 'react-router-dom';
 import { GitHubSVG } from '../assets/svg/github';
 import { DropDownMenu } from '../drop-down-menu';
 import { ProfileAvatar } from '../profile-avatar';
 import { SwitchPhotos } from '../switch-photos';
+import { logOut } from '../../store/auth';
+import { useAppDispatch } from '../../hooks/react-redux';
 
 import './header.css';
 
-export const Header = () => (
-  <header>
-    <div className="header-container">
-      <GitHubSVG />
-      <SwitchPhotos />
-      <DropDownMenu>
-        <ProfileAvatar avatarModifier="downArrow" />
-      </DropDownMenu>
-    </div>
-  </header>
-);
+export const Header = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const menuList = [
+    { id: 1, clickHandler: () => navigate('/update-profile'), text: 'Update profile' },
+    { id: 0, clickHandler: () => dispatch(logOut()), text: ' Sign Out' },
+  ];
+
+  return (
+    <header>
+      <div className="header-container">
+        <GitHubSVG />
+        <SwitchPhotos />
+        <DropDownMenu menuList={menuList}>
+          <ProfileAvatar avatarModifier="downArrow" />
+        </DropDownMenu>
+      </div>
+    </header>
+  );
+};
